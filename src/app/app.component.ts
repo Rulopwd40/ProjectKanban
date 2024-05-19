dfff638be02c5ff2613ece31fb10e80aca0592d1
rstby1 <diegoandreslemercier@gmail.com>
2024-05-19 16:18:10 -0300

    Card fixes
    *Reworked style for cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BoardComponent } from './board/board.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
